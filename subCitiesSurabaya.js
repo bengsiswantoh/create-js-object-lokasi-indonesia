52109dea32b282c411787c9c29f47c30d0e096f0
Bengsiswanto <bengsiswantoh@gmail.com>
2022-09-16 11:14:03 +0700

    fix because web changed

--- subCitiesSurabaya.js
+++ subCitiesSurabaya.js
@@ -1,38 +1,47 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs");
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const ObjectsToCsv = require('objects-to-csv');
 
-const url = "https://surabaya.go.id/id/page/0/8166/kecamatan";
-const filename = "surabaya-districts.js";
+const url = 'https://surabaya.go.id/id/page/0/8166/kecamatan';
+const filename = 'surabaya-sub-city';
+
+const writeCsv = async (districtsArr) => {
+  const csv = new ObjectsToCsv(districtsArr);
+  await csv.toDisk(`${filename}.csv`);
+};
+
+const writeJs = (districts) => {
+  const content = `export const surabayaSubCities = ${JSON.stringify(
+    districts
+  )}`;
+  fs.writeFileSync(`${filename}.js`, content);
+};
 
 const main = async () => {
   const { data } = await axios({
-    method: "get",
+    method: 'get',
     url,
   });
 
   const districts = {};
+  const districtsArr = [];
 
   let selector = cheerio.load(data);
-  const first_row = selector("div.row:first");
 
-  selector = cheerio.load(first_row.html());
-  const items = selector("li > a");
+  const items = selector('li > a');
 
-  items.each(function (index, e) {
-    const districtElement = selector(this);
+  items.each((index, e) => {
+    const districtElement = selector(e);
     const district = districtElement.text().toUpperCase();
-
     const subCityElement = districtElement.parent().parent().parent();
-    const subCity = subCityElement.text().split("\n")[0].toUpperCase();
-
+    const subCity = subCityElement.text().split('\n')[0].toUpperCase();
     districts[district] = subCity;
-    console.log(`add ${district} = ${subCity}`);
+    districtsArr.push({ district, subCity });
   });
 
-  var content = `export const surabayaDistricts = ${JSON.stringify(districts)}`;
-
-  fs.writeFileSync(filename, content);
+  writeCsv(districtsArr);
+  writeJs();
 };
 
 main();
